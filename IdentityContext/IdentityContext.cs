781cf2f212247aaf4ae736701ec939d1b077fe03
mohammedatefa <mohamed.atef.abdelsbor.com>
2023-11-12 15:14:29 +0200

    create users account controller and handle the end points of Login And Registeration using Identity &Token Services

---
+++ IdentityContext/IdentityContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TazaFood_Core.IdentityModels;
+
+namespace TazaFood_Repository.IdentityContext
+{
+    public class IdentityContext:IdentityDbContext<AppUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+        
+    }
+}
